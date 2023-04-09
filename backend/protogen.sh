cef6782dc7f266753be6c4069d0ca32928e5d188
Vasilis Danias <vasilis@bitloops.com>
2023-04-09 13:52:54 +0300

    Updated protogen.sh

--- backend/protogen.sh
+++ backend/protogen.sh
@@ -3,8 +3,10 @@
 IN_DIR="./src/proto"
 OUT_DIR="./src/proto/generated"
 
-# Generate the types
-yarn proto-loader-gen-types \
-    --grpcLib=@grpc/grpc-js \
-    --outDir=${OUT_DIR}/ \
-    ${IN_DIR}/*.proto
+# Generate the the JS and TS files
+protoc --ts_out=${OUT_DIR} --ts_opt=target=node --js_out=import_style=commonjs,binary:${OUT_DIR} -I ${IN_DIR} ${IN_DIR}/*.proto
+
+# Troubleshooting
+
+# If you get an error like this: "protoc-gen-ts: program not found or is not executable"
+# then you might need to install the protoc-gen-ts plugin as a global packge: npm install -g protoc-gen-ts
